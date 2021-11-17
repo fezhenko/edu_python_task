bc6ef8cca8583b238256d575fccb848cceabaae0
Aliaksei Fezhanka <fezhenko@gmail.com>
2021-11-17 16:09:54 +0300

    refactored db, tag_counter, pickling, started creating of gui for tag_counter.

--- backend_db.py
+++ backend_db.py
@@ -1,13 +1,12 @@
 import sqlite3
-
-global conn
-global curs
+import tag_counter
+import pickle_the_data
 
 
 def connect():
     conn = sqlite3.connect('tag_counter.db')
     curs = conn.cursor()
-    curs.execute("CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY, Site_name text, URL text, check_date "
+    curs.execute("CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY, Site_name text, url text, check_date "
                  "timestamp, 'tag_data' VARCHAR)")
     conn.commit()
     conn.close()
@@ -22,16 +21,64 @@ def view():
     return rows
 
 
-def add_pickled_dict_to_db(tag_data, Site_name="", URL="", check_date=""):
+def insert(tag_data, site_name="", url="", check_date=""):
     conn = sqlite3.connect('tag_counter.db')
     curs = conn.cursor()
     curs.execute(f"SELECT tag_data FROM tags WHERE tag_data='{tag_data}'")
     if curs.fetchone() is None:
-        curs.execute("""INSERT INTO tags VALUES(NULL,?,?,?,?)""", (tag_data, Site_name, URL, check_date))
+        curs.execute("""INSERT INTO tags VALUES(NULL,?,?,?,?)""", (tag_data, site_name, url, check_date))
         print("Pickled dictionary with tags has been added to tag_counter.db")
     else:
         print("Pickled_dict has been already added to warehouse :)")
     conn.commit()
     conn.close()
 
+
+def show_dict_from_db(tag_data):
+    conn = sqlite3.connect('tag_counter.db')
+    curs = conn.cursor()
+    curs.execute("SELECT tag_data FROM tags WHERE tag_data=?", (tag_data,))
+    rows = curs.fetchall()
+    conn.close()
+    return rows
+
+
+def view_urls():
+    conn = sqlite3.connect('tag_counter.db')
+    curs = conn.cursor()
+    curs.execute("SELECT * FROM tags WHERE url!=?", ('',))
+    rows = curs.fetchall()
+    conn.close()
+    return rows
+
+
+def insert_url(url):
+    conn = sqlite3.connect('tag_counter.db')
+    curs = conn.cursor()
+    curs.execute("SELECT url FROM tags WHERE url=?", (url,))
+    if curs.fetchone() is None:
+        curs.execute("""INSERT INTO tags VALUES(NULL,NULL,?,NULL,NULL)""", (url,))
+    else:
+        print("This URL has been already added to the list")
+    conn.commit()
+    conn.close()
+
+
 connect()
+
+
+
+
+
+
+
+
+
+# pickling_dict = pickle_the_data.pickling_the_dictionary(tag_counter.parse())
+# insert(pickling_dict, url='newtext')
+# insert_url('newtext')
+#
+# def urls():
+#     return [row[2] for row in view_urls()]
+#
+# rint(urls())
--- tag_reader_frontend.py
+++ tag_reader_frontend.py
@@ -1,25 +1,46 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import backend_db
+import tag_counter
+import pickle_the_data
 from timeit import timeit
 
 
+def get_selected_row(event):
+    return combo.get()
 
-def combo_values(*x):
-    values = [i for i in x if isinstance(i, str)]
-    return values
+
+def load_command():
+    # нужно добавить проверку что если у нас есть значение из комбобокса мы использеум его урл, в ином случае урл из энтри бокса
+    # нужно как-то задизейблить ентри если выбран комбобокс и наоборот
+    pickling_dict = pickle_the_data.pickling_the_dictionary(tag_counter.parse(url_text.get()))
+    backend_db.insert(pickling_dict)
+    backend_db.insert_url(url_text.get())
+    url_entry.delete(0, END)
+
+
+def combobox_urls():
+    return [row[2] for row in backend_db.view_urls()]
+
+
+def show_from_db():
+    messagebox.showinfo('All found tags on the loaded page', 'Текст')
+
 
 def view_command():
-    #view all from db
-
-
-# def start_and_finish():
-#     timeit module ??
+    pass
 
 
 window = Tk()
-combo_text = StringVar()
-combo = ttk.Combobox(window, postcommand=view_command, state="readonly", width=50)
+
+window.wm_title('Tag counter')
+
+opts = StringVar()
+combo = ttk.Combobox(window, textvariable=opts, width=50)
+combo['values'] = combobox_urls()
 combo.grid(row=0, column=0, columnspan=2)
+combo.bind("<<ComboboxSelected>>", get_selected_row)
 
 url_text = StringVar()
 url_entry = Entry(window, textvariable=url_text, width=53)
@@ -28,12 +49,10 @@ url_entry.grid(row=1, column=0, columnspan=2)
 pb = ttk.Progressbar(window, orient='horizontal', mode='determinate', length=322)
 pb.grid(row=3, column=0, columnspan=2)
 
-load_button = Button(window, text='Load', width=20, command=pb.start)
+load_button = Button(window, text='Load', width=20, command=pb.start and load_command)
 load_button.grid(row=2, column=0)
 
-showfromdb_button = Button(window, text='Show from DB', command=pb.start, width=23)
+showfromdb_button = Button(window, text='Show from DB', command=pb.start and show_from_db, width=23)
 showfromdb_button.grid(row=2, column=1)
 
 window.mainloop()
-
-# print(combo_values('Hello', "Byebye"))
