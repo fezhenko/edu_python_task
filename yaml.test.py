dae824629e48f46ce83c495ae27362e1ca1d4641
Aliaksei Fezhanka <fezhenko@gmail.com>
2022-01-10 00:31:36 +0300

    refactored add_synonym function

--- yaml.test.py
+++ yaml.test.py
@@ -7,17 +7,57 @@ def read_yaml(filepath):
         return data
 
 
-def add_to_yaml(filepath, data): #пока что перезаписывает файл.
-    with open(filepath, "w") as file:
+def add_to_yaml(filepath, data):
+    with open(filepath, "a") as file:
         yaml.dump(data, file)
 
 
+def create_new_synonym(key, value):
+    dict = {}
+    dict[key] = value
+    return dict
+
+
+def delete(filepath, key):
+    with open(filepath, 'r') as file:
+        content = yaml.safe_load(file)
+        list_of_keys = list(content.keys())
+        print(list(content.keys()))
+        if key in list_of_keys:
+            del content[key]
+            with open(filepath, 'w') as new_file:
+                yaml.dump(content, new_file)
+        else:
+            print(f"{key} is not in 'synonyms.yaml'")
+
+
 path = "synonyms.yaml"
-data2 = {"sword": 100, "qweerty": 5565}
+delete(path, 'bing')
 
-q = read_yaml(path)
-qwe = [i for i in q.keys()]
-new_item = [i for i in data2.keys() if i not in qwe]
+# def create_synonym(self, key, value):
+#     my_dict = {key: value}
+#     url = my_dict[key]
+#     if key in self.data:
+#         print(f"'{key}' already in the list of synonyms")
+#         return url
+#     else:
+#         if not re.match('(?:http|ftp|https)://', my_dict[key]):
+#             updated_url = str('https://{}'.format(my_dict[key]))
+#             print(f"{my_dict[key]} is updated {updated_url}")
+#             my_dict[key] = updated_url
+#             with open(self.filepath, "a") as file:
+#                 yaml.dump(my_dict, file)
+#                 print(f"synonym '{key}' has been added for {my_dict[key]}")
+#             return updated_url
+#         else:
+#             print(f"{my_dict[key]} is fine as url")
+#             return url
 
-add_to_yaml(path,new_item)
-print(q)
+
+# data2 = {'sword': 'https://google.com'}
+#
+# synonyms = read_yaml(path)
+# synonyms_keys = [i for i in synonyms.keys()]
+#
+# new_dict = create_new_synonym('hello', 'https://www.youtube.com/')
+# add_to_yaml(path, new_dict)
--- yaml_reader.py
+++ yaml_reader.py
@@ -1,5 +1,6 @@
 import yaml
 import re
+from datetime import datetime
 
 
 class yaml_reader:
@@ -8,33 +9,67 @@ class yaml_reader:
         with open(self.filepath, "r") as f:
             self.data = yaml.safe_load(f)
 
-    def check_synonym(self, value):
-        if value in list(self.data.keys()):
-            return self.data[value]
+    def check_synonym(self, sname, svalue):
+        if sname in self.data and self.data[sname] == svalue:
+            print(self.data[sname])
+            return self.data[sname]
+        elif sname in self.data and self.data[sname] != svalue:
+            print(
+                f"'{sname}' in the list of synonyms, but value in the list:{self.data[sname]}, and your value is {svalue}")
+        else:
+            print(f"'{sname}','{svalue}' are not in the list of synonyms")
+
+    def add_synonym(self, key, value):
+        """добавляет ключ и значение синонима в ямл файл"""
+        if key in self.data and self.data[key]['synonym_value'] == value:
+            print(f"'{value}' as '{key}' is already in the list of synonyms")
+            return self.data[key]['synonym_value']
         else:
             if not re.match('(?:http|ftp|https)://', value):
                 updated_url = str('https://{}'.format(value))
-                return updated_url
-
-    def create_synonym(self, key, value):
-        """Создаёт словарь с синонимом и ссылкой для этого синонима и добавляет его в ямл файл"""
-        my_dict = {key: value}
-        url = my_dict[key]
-        if key in self.data:
-            print(f"'{key}' already in the list of synonyms")
-            return url
-        else:
-            if not re.match('(?:http|ftp|https)://', my_dict[key]):
-                updated_url = str('https://{}'.format(my_dict[key]))
-                print(f"{my_dict[key]} is updated {updated_url}")
-                my_dict[key] = updated_url
-                with open(self.filepath, "a") as file:
-                    yaml.dump(my_dict, file)
-                    print(f"synonym '{key}' has been added for {my_dict[key]}")
+                self.data[key] = {'synonym_name': key, 'synonym_value': updated_url,
+                                  'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+                with open(self.filepath, "w") as file:
+                    yaml.dump(self.data, file)
+                print(f"{value} is updated {updated_url}")
+                print(f"synonym '{key}' has been added for {updated_url}")
                 return updated_url
             else:
-                print(f"{my_dict[key]} is fine as url")
-                return url
+                self.data[key] = {'synonym_name': key, 'synonym_value': value,
+                                  'created': datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+                with open(self.filepath, "w") as file:
+                    yaml.dump(self.data, file)
+                print(f"{value} is fine as url")
+                print(f"{value} has been added to the list of synonyms as '{key}'")
+                return value
+
+    # def check_synonym(self, value):
+    #     if value in list(self.data.keys()):
+    #         return self.data[value]
+    #     else:
+    #         if not re.match('(?:http|ftp|https)://', value):
+    #             updated_url = str('https://{}'.format(value))
+    #             return updated_url
+
+    # def create_synonym(self, key, value):
+    #     """Создаёт словарь с синонимом и ссылкой для этого синонима и добавляет его в ямл файл"""
+    #     my_dict = {key: value}
+    #     url = my_dict[key]
+    #     if key in self.data:
+    #         print(f"'{key}' already in the list of synonyms")
+    #         return url
+    #     else:
+    #         if not re.match('(?:http|ftp|https)://', my_dict[key]):
+    #             updated_url = str('https://{}'.format(my_dict[key]))
+    #             print(f"{my_dict[key]} is updated {updated_url}")
+    #             my_dict[key] = updated_url
+    #             with open(self.filepath, "a") as file:
+    #                 yaml.dump(my_dict, file)
+    #                 print(f"synonym '{key}' has been added for {my_dict[key]}")
+    #             return updated_url
+    #         else:
+    #             print(f"{my_dict[key]} is fine as url")
+    #             return url
 
     def delete_synonym(self, key):
         with open(self.filepath, 'r') as file:
@@ -55,8 +90,5 @@ class yaml_reader:
 
 
 y = yaml_reader()
-y.create_synonym('bing', 'bing.com')
-y.delete_synonym('bing')
-y.view_synonyms()
-
+y.add_synonym('bing(correct_url)', 'https://bing.com')
 # (ru|com|by|net$)
