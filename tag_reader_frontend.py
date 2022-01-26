14aa8c99acc92c20c329f450d29fd5e2c46827c1
Aliaksei Fezhanka <fezhenko@gmail.com>
2022-01-26 18:51:45 +0300

    adjust fronend side.

--- tag_reader_frontend.py
+++ tag_reader_frontend.py
@@ -15,22 +15,31 @@ class Window(object):
 
         self.combo = ttk.Combobox(window, textvariable=self.opts, width=50)
         self.combo['values'] = self.combobox_urls()
-        self.combo.grid(row=0, column=0, columnspan=2, sticky=W + E, padx=1, pady=5)
+        self.combo.grid(row=0, column=0, columnspan=4, sticky=W + E, padx=1, pady=3)
         self.combo.bind("<<ComboboxSelected>>", self.get_selected_row)
 
         self.url_text = StringVar()
         self.url_entry = Entry(window, textvariable=self.url_text, width=50)
-        self.url_entry.grid(row=1, column=0, columnspan=2, sticky=W + E, padx=1, pady=5)
+        self.url_entry.grid(row=1, column=0, columnspan=4, sticky=W + E, padx=1, pady=3)
 
         self.pb = ttk.Progressbar(window, orient='horizontal', mode='determinate')
-        self.pb.grid(row=3, column=0, columnspan=2, sticky=W + E)
+        self.pb.grid(row=3, column=0, columnspan=4, sticky=W + E, pady=3)
 
-        self.load_button = Button(window, text='Load', width=20, command=self.load_command)
-        self.load_button.grid(row=2, column=0, sticky=W)
+        self.load_button = Button(window, text='Load', width=15, command=self.load_command)
+        self.load_button.grid(row=2, column=0, sticky=W, pady=1)
 
         self.showfromdb_button = Button(window, text='Show from DB', command=self.show_from_db,
-                                        width=20)
-        self.showfromdb_button.grid(row=2, column=1, sticky=E)
+                                        width=15)
+        self.showfromdb_button.grid(row=2, column=1, sticky=E, pady=1)
+
+        self.add_synonym_button = Button(window, text='Add Synonym', width=15)
+        self.add_synonym_button.grid(row=2, column=2, columnspan=2,sticky=E, pady=1)
+
+        self.l1 = Label(window, text='List of Synonyms')
+        self.l1.grid(row=4, column=0, columnspan=4, sticky=W, pady=0)
+
+        self.list1 = Listbox(window)
+        self.list1.grid(row=5, column=0, columnspan=2,  rowspan=5, sticky=W+E)
 
 
 
@@ -38,8 +47,6 @@ class Window(object):
         return self.combo.get()
 
     def load_command(self):
-        # нужно добавить проверку что если у нас есть значение из комбобокса мы использеум его урл, в ином случае урл из энтри бокса
-        # нужно как-то задизейблить ентри если выбран комбобокс и наоборот
         if self.combo.get():
             for i in range(101):
                 self.pb.configure(value=i)
@@ -78,8 +85,6 @@ class Window(object):
             messagebox.showinfo(f"{tags}")
             self.url_entry.delete(0, END)
 
-    def view_command(self):
-        pass
 
 
 window = Tk()
